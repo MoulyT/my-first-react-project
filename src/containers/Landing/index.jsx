243e2331f581f09b2c96f96d4a12b6f2f6b6b918
MoulyT <109950811+MoulyT@users.noreply.github.com>
2022-10-26 12:44:43 +0200

    Solved bug line-height. Finished intro

--- src/containers/Landing/index.jsx
+++ src/containers/Landing/index.jsx
@@ -1,16 +1,30 @@
 import Button from "../../components/Button";
+import { ReactComponent as ImageIntro } from "../../assets/landing/images/illustration-intro.svg";
+import "./styles.scss";
 
 const Landing = () => {
   return (
     <>
       <section className="introduction wrapper">
-        <div className="introcution__text">
+        <div className="introduction__text">
           <h1>Bring everyone together to build better products.</h1>
           <p>
             Manage makes it simple for software teams to plan day-to-day while
             keeping larger team goals in view.
           </p>
           <Button>Get started</Button>
+        </div>
+
+        <ImageIntro className="introduction__img" />
+      </section>
+      <section>
+        <div>
+          <h2>Whats different about Manage?</h2>
+          <p>
+            Manage provides all the functionality your team needs, without the
+            complexity. Our software is tailor-made for modern digital product
+            teams.
+          </p>
         </div>
       </section>
     </>
